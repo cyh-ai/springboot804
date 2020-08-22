c38e31a4ee5a2f91b4050fbe7041365df49164f5
wangdengke <1123615523@qq.com>
2020-08-22 11:48:41 +0800

    第二次

--- src/main/java/com/aaa/test/TestController.java
+++ src/main/java/com/aaa/test/TestController.java
@@ -2,10 +2,9 @@ package com.aaa.test;/*
  */
 
 public class TestController {
-
     public void add(){
+        System.out.println("第二次提交");
         System.out.println("wucoanima ");
         System.out.println("无情看哈拉少！");
     }
-
 }
